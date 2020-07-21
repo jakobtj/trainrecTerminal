e7f458f30af776e3446c9caf3515b71268719fcd
Jakob <jjt@fastmail.com>
2020-07-21 21:16:06 +0200

    Initiate reader for CSV storage in main

--- src/main/java/no/trainrec/terminal-interface/CommandLineMain.java
+++ src/main/java/no/trainrec/terminal-interface/CommandLineMain.java
@@ -2,9 +2,24 @@ package no.trainrec.terminal_interface;
 
 import java.util.Scanner;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 public class CommandLineMain {
     public static void main(String[] argv) {
-        CSVStorage storage = new CSVStorage();
+        try {
+            File data = new File("data.csv");
+            data.createNewFile(); // Does nothing if file already exists
+            BufferedReader reader = new BufferedReader(new FileReader(data));
+            CommandLineMain.run(reader);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void run(BufferedReader reader) {
+        CSVStorage storage = new CSVStorage(reader);
         TextInterface ui = new TextInterface(new CoreAccessor(storage));
         CommandParser parser = new CommandParser("");
         Scanner input = new Scanner(System.in);
